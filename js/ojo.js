79e9f0736e49648a61222896104c5a951f86a31c
Ra5454 <lazarus6549@gmail.com>
2021-02-28 21:53:55 +0100

    Update33

--- js/ojo.js
+++ js/ojo.js
@@ -12,23 +12,23 @@ function mostrarPassword(){
 	$(document).ready(function () {
 	//CheckBox mostrar contraseña
 	$('#ShowPassword').click(function () {
-		$('#Password').attr('type', $(this).is(':checked') ? 'text' : 'password');
+		$('#password1').attr('type', $(this).is(':checked') ? 'text' : 'password');
 	});
 });
 function mostrarPassword2(){
-		var cambio = document.getElementById("txtPassword2");
-		if(cambio.type == "password"){
-			cambio.type = "text";
-			$2('.icon').removeClass('fa fa-eye-slash').addClass('fa fa-eye');
+		var cambio2 = document.getElementById("txtPassword2");
+		if(cambio2.type == "password"){
+			cambio2.type = "text";
+			$('.icon2').removeClass('fa fa-eye-slash').addClass('fa fa-eye');
 		}else{
-			cambio.type = "password";
-			$2('.icon').removeClass('fa fa-eye').addClass('fa fa-eye-slash');
+			cambio2.type = "password";
+			$('.icon2').removeClass('fa fa-eye').addClass('fa fa-eye-slash');
 		}
 	} 
 	
-	$2(document).ready(function () {
+	$(document).ready(function mostrarPassword2 () {
 	//CheckBox mostrar contraseña
-	$2('#ShowPassword').click(function () {
-		$2('#Password').attr('type', $2(this).is(':checked') ? 'text' : 'password');
+	$('#ShowPassword').click(function mostrarPassword2() {
+		$('#Password').attr('type', $(this).is(':checked') ? 'text' : 'password');
 	});
 });
